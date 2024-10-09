f28e0882f4628e7933595ff6975edb5e30c1614b
Xharry01X <harshitsingh50621@gmail.com>
2024-10-09 09:37:03 +0530

    lets see if this works..

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 const fs = require('fs');
 const plist = require('plist');
 
-// Define the data for the plist file, including Shortcuts actions
-const data = {
-  Title: 'Hello World',
-  Message: 'This is a simple plist example in Node.js',
-  Version: 1.0,
-  WFWorkflowActions: [
-    {
-      WFWorkflowActionIdentifier: 'is.workflow.actions.showalert',
-      WFWorkflowActionParameters: {
-        WFAlertActionTitle: 'Alert',
-        WFAlertActionMessage: 'This is an alert action from a shortcut.'
-      }
-    },
-    {
-      WFWorkflowActionIdentifier: 'is.workflow.actions.gettext',
-      WFWorkflowActionParameters: {
-        WFTextActionText: 'Hello from Shortcuts!'
-      }
-    }
-  ],
-  WFWorkflowClientRelease: '2.1.2',
-  WFWorkflowClientVersion: '1000',
-  WFWorkflowImportQuestions: [],
-  WFWorkflowTypes: ['NCWidget', 'WatchKit', 'Normal'],
+/**
+ * Create a shortcut file and save it as a .shortcut plist.
+ * @param {Object} jsonData - The JSON data representing the shortcut.
+ * @param {string} fileName - The name of the file to save (without extension).
+ */
+function createShortcutFile(jsonData, fileName) {
+    // Convert JSON to plist format
+    const plistString = plist.build(jsonData);
+    
+    // Write the plist string to a .shortcut file
+    fs.writeFileSync(`${fileName}.shortcut`, plistString, 'utf8');
+    console.log(`Shortcut file saved as ${fileName}.shortcut`);
+}
+
+// Example JSON data for the shortcut
+const jsonData = {
+    name: "My Shortcut",
+    actions: [
+        {
+            type: "Get Latest Photos",
+            parameters: {
+                count: 5
+            }
+        },
+        {
+            type: "Show Result",
+            parameters: {
+                text: "Here are your latest photos!"
+            }
+        }
+    ]
 };
 
-// Convert the JavaScript object to a plist format
-const plistData = plist.build(data);
-
-// Write the plist data to a file
-fs.writeFileSync('hello_world.plist', plistData);
-
-console.log('Plist file created: hello_worldIn.plist');
+// Create the shortcut file
+createShortcutFile(jsonData, 'MyShortcut');
